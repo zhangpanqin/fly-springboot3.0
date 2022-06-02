11af9cab3bebc3829d597d4e1b86a65807a3635b
zhangpanqin <zhangpanqin@outlook.com>
2022-06-02 23:27:35 +0800

    build(N/A): 初始化项目

---
+++ example/src/main/java/com/mflyyou/example/user/UserController.java
@@ -0,0 +1,14 @@
+package com.mflyyou.example.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String demo() {
+        return "success";
+    }
+}

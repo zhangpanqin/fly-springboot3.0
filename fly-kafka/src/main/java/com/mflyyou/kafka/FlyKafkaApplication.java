7695d1b70e3c790b0ae783eb43f114f49b5c5118
zhangpanqin <zhangpanqin@outlook.com>
2022-06-22 21:54:44 +0800

    fix: add kafka

---
+++ fly-kafka/src/main/java/com/mflyyou/kafka/FlyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mflyyou.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyKafkaApplication.class, args);
+    }
+
+}
---
+++ fly-kafka/src/test/java/com/mflyyou/kafka/FlyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mflyyou.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
